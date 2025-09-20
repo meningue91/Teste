591813e5b41226620a374519ea0a4004629409d0
anicolella <nicolella@usp.br>
2025-09-20 11:49:03 -0300

    Mais uma tentativa

--- script_inicial.R
+++ script_inicial.R
@@ -14,3 +14,6 @@ print("Oi João acho que deu certo agora tirei o compartilhamento")
 
 print("teste-teste")
 print("sjkdakldjaklsd")
+
+
+print("oi João, fiz o pull hoje e não deu erro, disse que estava upto date. 20/09.Vou fazer outro commit com alterações ")
